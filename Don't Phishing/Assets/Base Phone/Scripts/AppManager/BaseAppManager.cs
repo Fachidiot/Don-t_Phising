beb28b5a45fb7e09a7b65f371faa352e308d3335
Fachidiot <54926396+Fachidiot@users.noreply.github.com>
2025-04-15 01:10:28 +0900

    # Base Phone seperate

---
+++ Don't Phishing/Assets/Base Phone/Scripts/AppManager/BaseAppManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public abstract class BaseAppManager : MonoBehaviour
+{
+    [SerializeField]
+    private string m_AppName;
+
+    public void InitApp()
+    {
+        if (m_AppName == string.Empty)
+            m_AppName = gameObject.name;
+    }
+
+    public abstract void SetText();
+
+    public abstract void ResetApp();
+}
--- Don't Phishing/Assets/Scripts/Apps/BaseAppManager.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class BaseAppManager : MonoBehaviour
-{
-    public abstract void SetText();
-
-    public abstract void ResetApp();
-}
